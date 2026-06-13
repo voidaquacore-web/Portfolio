22ae3fa2c29c0d1bc52328720b142f59bd352e2e
aqua <voidaquacore@gmail.com>
2026-06-13 16:11:02 +0545

    optimize responsiveness

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+        },
+      },
+    },
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+    cssCodeSplit: true,
+  },
+  server: {
+    headers: {
+      'Cache-Control': 'public, max-age=3600',
+    },
+  },
 })
