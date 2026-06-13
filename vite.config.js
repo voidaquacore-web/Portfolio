157763f710af0f854c69c865108451a4f6d11e67
aqua <voidaquacore@gmail.com>
2026-06-13 16:54:37 +0545

    attempt4

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssCodeSplit: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        },
-      },
-    },
+    cssCodeSplit: true,
   },
-  server: {
-    headers: {
-      'Cache-Control': 'public, max-age=3600',
-    },
-  },
-  assetsInclude: ['**/*.webp', '**/*.png', '**/*.jpg'],
 })
